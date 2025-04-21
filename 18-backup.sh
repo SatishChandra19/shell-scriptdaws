0debd4a3ad64c2237f0996ad42db57c75e85fb06
Satishchandra19 <satishchandra1997@gmail.com>
2025-04-21 16:33:47 +0100

    expense shell

--- 18-backup.sh
+++ 18-backup.sh
@@ -53,4 +53,9 @@ echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
 FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
 
-echo "Files are: $FILES"
+if [ -n "$FILES" ]
+then 
+    echo "Files are: $FILES"
+else
+    scho "No files found older that $DAYS to zip"
+fi
